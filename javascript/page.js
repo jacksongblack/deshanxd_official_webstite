b630e7b86f6fdc300baf9002acdaec2dae712ed7
jacksongblack <songyuchaoemail@163.com>
2014-09-05 17:12:15 +0800

    上传首页页面

---
+++ javascript/page.js
@@ -0,0 +1,22 @@
+function imageAnimate(img){
+    $.each(  [
+        { opacity:"0.1"},
+        { opacity:"0.3"},
+        { opacity:"0.5"},
+        { opacity:"0.7"},
+        { opacity:"0.9"},
+        { opacity:"1"}
+    ],function(index,v){
+        img.animate(v,"slow"); 
+    });  
+
+}
+
+(function($){
+    $(document).ready( function(){ 
+        imageAnimate($(".page-logo"));
+        setTimeout("imageAnimate($('.page-home-link'))",1000);
+    });
+})(jQuery);
+
+
