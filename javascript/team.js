8e6f64b9c0cf0e9ecdc370c6a556578720b78e46
jacksongblack <jacksongemail@163.com>
2014-09-09 00:46:09 +0800

    增加企业文化页面

--- javascript/team.js
+++ javascript/team.js
@@ -3,10 +3,36 @@
         function(){
         $(".page-team-menber a").click(function(){
         $(".page-team-list").show();
+        $(".page-team-list-background").show();
         });
         $(".page-team-close").click(function(){
             $(".page-team-list").hide();
+            $(".page-team-list-background").hide();
         });
+        $(".page-team-head-list a").hover(
+            function(){
+            var hover_link = this;
+                $(".page-team-head-list a").each(
+                    function(){
+                      if(this === hover_link){
+                      $(this).attr("style","opacity:1;");
+                }else{
+                      $(this).attr("style","opacity:0.5;");
+                }
+                }
+            ); 
+            var img_url = $(this).attr("data-img");
+            $(".page-team-img img").each(
+                function(){
+                if ($(this).attr("src") === img_url ){
+                    $(this).show();
+                }else{
+                    $(this).hide();
+                } 
+            } 
+            );  
+        }  
+        );
     }
-    )
+    );
 })(jQuery);
